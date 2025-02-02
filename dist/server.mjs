1c81f203e02bc56da38344f7dd561e31fba7c4cc
Matheus Estevam <matheusestevam421@gmail.com>
2025-02-02 20:53:25 -0300

    mudança na porta3

--- dist/server.mjs
+++ dist/server.mjs
@@ -10,8 +10,9 @@ import "./chunk-Y4O45TBE.mjs";
 import "./chunk-6WLVXSUQ.mjs";
 
 // src/server.ts
+var PORT = Number(process.env.PORT);
 try {
-  app_default.listen({ port: 4e3 });
+  app_default.listen({ port: PORT || 4e3 });
 } catch (error) {
   console.error("Error starting the server:", error);
   process.exit(1);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
-
+const PORT = Number(process.env.PORT);
 try {
-	app.listen({ port: 4000 });
+	app.listen({ port: PORT || 4000 });
 } catch (error) {
 	console.error('Error starting the server:', error);
 	process.exit(1);
